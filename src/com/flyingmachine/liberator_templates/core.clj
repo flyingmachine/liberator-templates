3fe234857b2b4a75c3ab27d6e9c0a3403f3ed409
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-19 18:23:32 -0500

    abbreviations

--- src/com/flyingmachine/liberator_templates/core.clj
+++ src/com/flyingmachine/liberator_templates/core.clj
@@ -1,11 +1,9 @@
 (ns com.flyingmachine.liberator-templates.core
   (:require [liberator.core :refer (defresource)]))
 
-(def expanders {})
-
 (defn expand
-  [action [key form]]
-  (if-let [expander (get expanders key)]
+  [action abbreviations [key form]]
+  (if-let [expander (get abbreviations key)]
     (expander action form)
     [key form]))
 
@@ -21,7 +19,7 @@
       shared-paramlist)))
 
 (defn expand-decisions
-  [resource-name decisions]
+  [abbreviations resource-name decisions]
   (if-not (empty? decisions)
     (reduce into (map #(expand (keyword resource-name) %)
                       (partition 2 decisions)))))
@@ -31,11 +29,12 @@
   `(defmacro ~template-name
      [& decisions#]
      (let [paramlist# (paramlist (quote ~(:paramlist shared-options)) decisions#)
+           decision-abbreviations# (quote ~(:decision-abbreviations shared-options))
            resource-name# (quote ~(resource-name template-name))]
        `(defresource ~resource-name# ~paramlist#
           ~~@(:shared-decisions shared-options)
           ~~@template-decisions
-          ~@(expand-decisions resource-name# decisions#)))))
+          ~@(expand-decisions decision-abbreviations# resource-name# decisions#)))))
 
 (defmacro deftemplates
   [options & templates]
