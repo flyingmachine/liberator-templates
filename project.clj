ffe2b44f6e6bec69895b80169295d0e59f8fee14
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-20 10:43:43 -0500

    project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.flyingmachine/liberator-templates "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.flyingmachine/liberator-templates "0.1.0"
+  :description "Create macros which create liberator resources"
+  :url "https://github.com/flyingmachine/liberator-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
